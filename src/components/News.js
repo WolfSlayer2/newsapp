362008794a40528252ca124f94769641c2144928
Divyansh Asthana <shivammohan850@gmail.com>
2023-09-02 01:07:12 +0530

    Initial commit

---
+++ src/components/News.js
@@ -0,0 +1,79 @@
+import React, { Component } from 'react'
+import Newsitem from './Newsitem'
+import Spinner from './Spinner'
+import PropTypes from 'prop-types'
+
+export class News extends Component {
+    static defaultProps = {
+    country: 'in',
+    pageSize: 9,
+    category: 'general'
+  }
+  static propTypes = {
+    country: PropTypes.string,
+    pageSize: PropTypes.number,
+    category: PropTypes.string
+  }
+    constructor() {
+        super();
+        this.state = {
+            articles: [],
+            loading: false,
+            page: 1
+        }
+    }
+    async componentDidMount() {
+        let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=618a1f0348634d02bc350074e657b3fa&page=1&pageSize=${this.props.pageSize}`;
+        this.setState({ loading: true })
+        let response = await fetch(url);
+        let data = await response.json();
+        this.setState({ articles: data.articles, totalResults: data.totalResults, loading: false })
+    }
+
+    handlePrevClick = async () => {
+        let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=618a1f0348634d02bc350074e657b3fa&page=${this.state.page - 1}&pageSize=${this.props.pageSize}`;
+        this.setState({ loading: true })
+        let response = await fetch(url);
+        let data = await response.json();
+        this.setState({
+            page: this.state.page - 1,
+            articles: data.articles,
+            loading: false
+        })
+    }
+    handleNextClick = async () => {
+        let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=618a1f0348634d02bc350074e657b3fa&page=${this.state.page + 1}&pageSize=${this.props.pageSize}`;
+        this.setState({ loading: true })
+        let response = await fetch(url);
+        let data = await response.json();
+        this.setState({
+            page: this.state.page + 1,
+            articles: data.articles,
+            loading: false
+        })
+
+    }
+
+    render() {
+        return (
+            <div className='container my-3'>
+                <h2 className='text-center'>Top Headlines</h2>
+                {this.state.loading && <Spinner />}
+                <div className="row">
+                    {!this.state.loading && this.state.articles.map((ele) => {
+                        return <div className="col-md-4" key={ele.url}>
+                            <Newsitem title={ele.title ? ele.title.slice(0, 45) : ""} desc={ele.description ? ele.description.slice(0, 88) : ""} imgUrl={ele.urlToImage} newsUrl={ele.url} date={ele.publishedAt} source={ele.source.name} />
+                        </div>
+                    })}
+                </div>
+                <div className="container my-3 d-flex justify-content-between">
+                    <button disabled={this.state.page <= 1} type="button" className="btn btn-dark" onClick={this.handlePrevClick}>&larr; Prev</button>
+                    <button disabled={(this.state.page + 1 > Math.ceil(this.state.totalResults / this.props.pageSize))} type="button" className="btn btn-dark" onClick={this.handleNextClick}>Next &rarr;</button>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default News
+
---
+++ src/components/Newsitem.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react'
+
+export class Newsitem extends Component {
+  render() {
+    let { title, desc, imgUrl, newsUrl, date, source } = this.props;
+    return (
+      <div className='my-3 mx-3'>
+        <div className="card">
+          <span className="position-absolute top-0  translate-middle badge rounded-pill bg-secondary" style={{left:"95%",zIndex:'1'}}>
+            {source}
+          </span>
+          <img src={imgUrl ? imgUrl : "https://swarajya.gumlet.io/swarajya/2021-12/2deef8fa-67e7-4469-8a50-f0dfac9ebaca/ahddagfiugr.jpg?w=1200&auto=format%2Ccompress&ogImage=true"} className="card-img-top" alt="..." />
+          <div className="card-body">
+            <h5 className="card-title">{title}..</h5>
+            <p className="card-text">
+              {desc}...
+            </p>
+            <p className="card-text"><small className="text-body-secondary">Published at {new Date(date).toUTCString()}</small></p>
+            <a href={newsUrl} target='_blank' rel="noopener noreferrer" className="btn btn-primary">
+              Read More
+            </a>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Newsitem
